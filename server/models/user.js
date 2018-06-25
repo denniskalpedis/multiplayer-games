0f7e9514a63c6ba09366edb4f727978a88e94f72
Liz <elizabethmpedley@gmail.com>
2018-06-25 13:55:33 -0500

    built out backend more and created frontend service and component

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 var UserSchema = new mongoose.Schema({
-    email: {type: String, required: [true, "Email is required"],validate: [validateEmail, 'Please fill a valid email address'],},
+    email: {type: String, required: [true, "Email is required"],match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
     username: {type: String, required: [true, "Username is required"], minlength: [3, "Username must have at least 3 characters"]},
     password: {type: String,  required: [true, "Password is required."], minlength: [8, "Passwords must have at least 3 characters"]},
     wins: {}
