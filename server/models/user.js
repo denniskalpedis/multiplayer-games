bbf62e75855f96885081df1c61582e64549fbeb5
Liz <elizabethmpedley@gmail.com>
2018-06-25 13:06:42 -0500

    created user schema

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+var UserSchema = new mongoose.Schema({
+    email: {type: String, required: [true, "Email is required"],validate: [validateEmail, 'Please fill a valid email address'],},
+    username: {type: String, required: [true, "Username is required"], minlength: [3, "Username must have at least 3 characters"]},
+    password: {type: String,  required: [true, "Password is required."], minlength: [8, "Passwords must have at least 3 characters"]},
+    wins: {}
+   },{timestamp: true})
+   
+mongoose.model('User', UserSchema);
